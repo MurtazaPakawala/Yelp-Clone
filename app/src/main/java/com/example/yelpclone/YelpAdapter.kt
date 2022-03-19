13a5dec6c19ef9a5fb82e0e8ac629807a857c812
Murtaza Pakawala <mypaka52@gmail.com>
2022-03-20 01:47:28 +1100

    the app is working fine displaying the images and the restaurant info

--- app/src/main/java/com/example/yelpclone/YelpAdapter.kt
+++ app/src/main/java/com/example/yelpclone/YelpAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.yelpclone
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_restaurant.view.*
 
 class YelpAdapter(private val context: Context, private val data: List<YelpRes>) : RecyclerView.Adapter<YelpAdapter.ViewHolder>() {
@@ -24,6 +26,10 @@ class YelpAdapter(private val context: Context, private val data: List<YelpRes>)
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(restaurant: YelpRes) {
            itemView.tvName.text = restaurant.name
+            itemView.rb.rating =restaurant.rating.toFloat()
+            itemView.tvPrice.text = restaurant.price
+            itemView.tvRating.text = "Rating : ${restaurant.rating}"
+            Glide.with(context).load(restaurant.image_url).into(itemView.Image)
 
         }
     }
